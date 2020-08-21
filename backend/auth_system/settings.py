74ad7e1744fa8661dd25eab400601bedd3a3badb
mohammedsabah96 <mohammedsabah455@gmail.com>
2020-08-21 15:21:11 +0300

    Added login functionality

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -22,11 +22,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,4 +138,5 @@ DJOSER = {
     }
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
 AUTH_USER_MODEL = 'accounts.UserAccount'
